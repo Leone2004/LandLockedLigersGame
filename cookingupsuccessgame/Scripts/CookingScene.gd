301d965542ae7a1a2a1f4cf22fb36206e8587652
Noah Jorgensen <njorgensen@byui.edu>
2025-06-26 11:42:17 -0600

    added multiple pages to the recipe book

--- cookingupsuccessgame/Scripts/CookingScene.gd
+++ cookingupsuccessgame/Scripts/CookingScene.gd
@@ -1,6 +1,8 @@
 extends Node2D
 var ovenswitch : bool = true
 var recipe_book_visible : bool = false
+var current_recipe : int = 0
+@onready var recipe = get_node("CanvasLayer/RecipeBook/Current Recipe")
 @onready var recipe_book_layer = get_node("CanvasLayer/RecipeBook")
 @onready var camera : Camera2D = $Camera2D
 
@@ -15,3 +17,17 @@ func _on_button_pressed() -> void:
 func _on_recipe_pressed() -> void:
 	recipe_book_visible = !recipe_book_visible
 	recipe_book_layer.visible = recipe_book_visible
+
+
+func _on_next_pressed() -> void: # go to next page
+	current_recipe += 1
+	var recipe_number_text = "[center]Recipe Number %s[/center]"
+	recipe_number_text = recipe_number_text % current_recipe
+	recipe.text = recipe_number_text
+
+
+func _on_previous_pressed() -> void: #go to previous page
+	current_recipe -= 1
+	var recipe_number_text = "[center]Recipe Number %s[/center]"
+	recipe_number_text = recipe_number_text % current_recipe
+	recipe.text = recipe_number_text
