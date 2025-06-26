b3d8bcd78b85cdc5a18437c6bbe7c252904412f5
Noah Jorgensen <njorgensen@byui.edu>
2025-06-26 12:46:04 -0600

    created string formatting for recipes

--- cookingupsuccessgame/Scripts/CookingScene.gd
+++ cookingupsuccessgame/Scripts/CookingScene.gd
@@ -1,6 +1,7 @@
 extends Node2D
 var ovenswitch : bool = true
 var recipe_book_visible : bool
+var recipes = Array() # recipes[i][0] is name. recipes[i][1] is ingredients subarray
 var current_recipe : int
 @onready var recipe = get_node("CanvasLayer/RecipeBook/Current Recipe")
 @onready var recipe_book_layer = get_node("CanvasLayer/RecipeBook")
@@ -11,6 +12,7 @@ func _ready() -> void:
 	recipe_book_visible = false
 	recipe_book_layer.visible = recipe_book_visible
 	recipe.text = "[center][b]THE RECIPE BOOK[/b][/center]"
+	recipes.append(["pepperoni", ["pepperoni", "cheese", "sauce", "dough"]])
 
 func _on_button_pressed() -> void:
 	var screen_width := get_viewport().get_visible_rect().size.x
@@ -27,9 +29,10 @@ func _on_recipe_pressed() -> void:
 
 func _on_next_pressed() -> void: # go to next page
 	current_recipe += 1
-	var recipe_number_text = "[center]Recipe Number %s[/center]"
-	recipe_number_text = recipe_number_text % current_recipe
-	recipe.text = recipe_number_text
+	#var recipe_number_text = "[center]Recipe Number %s[/center]"
+	#recipe_number_text = recipe_number_text % current_recipe
+	var recipe_text = get_recipe_string(recipes[0])
+	recipe.text = recipe_text
 
 
 func _on_previous_pressed() -> void: #go to previous page
@@ -38,3 +41,12 @@ func _on_previous_pressed() -> void: #go to previous page
 		current_recipe -= 1
 		recipe_number_text = recipe_number_text % current_recipe
 		recipe.text = recipe_number_text
+
+func get_recipe_string(recipe) -> String:
+	var recipe_string = "[center][b]%s[/b][/center]\n"
+	recipe_string = recipe_string % recipe[0]
+	recipe_string += "[ul]\n"
+	for ingredient in recipe[1]:
+		recipe_string += "%s\n" % ingredient
+	recipe_string += "[/ul]"
+	return recipe_string
