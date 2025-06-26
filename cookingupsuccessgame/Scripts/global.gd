aaa3d5d0d750c2308ed487f63c5787b2e67262d5
Benflannery <Benflannery@byui.edu>
2025-06-26 14:27:58 -0600

    updated more ingredients

--- cookingupsuccessgame/Scripts/global.gd
+++ cookingupsuccessgame/Scripts/global.gd
@@ -4,4 +4,4 @@ var money : float = 100.00
 
 # Suggested New Variable System
 var ingredients = [0,0,0,0,0]
-var food = ["pepperoni", "cheese", "mushrooms", "sauce","pepper"]
+var food = ["pepperoni", "cheese", "mushrooms", "sauce","olives", "peppers"]
