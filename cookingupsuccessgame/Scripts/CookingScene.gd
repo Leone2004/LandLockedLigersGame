67c63928b8f37a86aaacefdfaa98e6550b1457c8
Noah Jorgensen <njorgensen@byui.edu>
2025-06-26 11:52:07 -0600

    added title page. prohibit recipes less than 1.

--- cookingupsuccessgame/Scripts/CookingScene.gd
+++ cookingupsuccessgame/Scripts/CookingScene.gd
@@ -1,10 +1,16 @@
 extends Node2D
 var ovenswitch : bool = true
-var recipe_book_visible : bool = false
-var current_recipe : int = 0
+var recipe_book_visible : bool
+var current_recipe : int
 @onready var recipe = get_node("CanvasLayer/RecipeBook/Current Recipe")
 @onready var recipe_book_layer = get_node("CanvasLayer/RecipeBook")
 @onready var camera : Camera2D = $Camera2D
+
+func _ready() -> void:
+	current_recipe = 0
+	recipe_book_visible = false
+	recipe_book_layer.visible = recipe_book_visible
+	recipe.text = "[center][b]THE RECIPE BOOK[/b][/center]"
 
 func _on_button_pressed() -> void:
 	var screen_width := get_viewport().get_visible_rect().size.x
@@ -27,7 +33,8 @@ func _on_next_pressed() -> void: # go to next page
 
 
 func _on_previous_pressed() -> void: #go to previous page
-	current_recipe -= 1
 	var recipe_number_text = "[center]Recipe Number %s[/center]"
-	recipe_number_text = recipe_number_text % current_recipe
-	recipe.text = recipe_number_text
+	if current_recipe > 1: # don't let go to non-existent recipes
+		current_recipe -= 1
+		recipe_number_text = recipe_number_text % current_recipe
+		recipe.text = recipe_number_text
