ad1cf063e3e4c4de5e9cf1f269e9c77b628595e0
Benflannery <Benflannery@byui.edu>
2025-07-01 11:42:05 -0600

    made all toppings w/ prices

--- cookingupsuccessgame/Scripts/global.gd
+++ cookingupsuccessgame/Scripts/global.gd
@@ -3,5 +3,5 @@ var day : int = 1
 var money : float = 100.00
 
 # Suggested New Variable System
-var ingredients = [0,0,0,0,0]
-var food = ["pepperoni", "cheese", "mushrooms", "sauce","olives", "peppers"]
+var ingredients = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+var food = ["pepperoni", "cheese", "mushrooms", "sauce", "olives", "peppers", "bacon", "ham", "anchovies", "garlic", "tomato", "onion", "sausage"]
