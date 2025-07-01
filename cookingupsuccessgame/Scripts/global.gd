655579c47f98dd59ef50ef47525f19cd84fceb70
Noah Jorgensen <njorgensen@byui.edu>
2025-07-01 13:31:01 -0600

    you can now add items to shopping list, and shopping list will update to show items

--- cookingupsuccessgame/Scripts/global.gd
+++ cookingupsuccessgame/Scripts/global.gd
@@ -4,5 +4,9 @@ var day: int = 1
 var money: float = 100.00
 
 # Suggested New Variable System
+
 var ingredients: Array[int] = [0, 0, 0, 0, 0]
 var food: Array[String] = ["pepperoni", "cheese", "mushrooms", "sauce", "pepper"]
+
+# Shopping List Global
+var shopping_list = Array() # shopping_list[i][0] is item, while shopping_list[i][1] is the ammount of the item
