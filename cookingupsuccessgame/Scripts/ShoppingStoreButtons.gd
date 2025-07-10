6a2023ab3c6861f97128f362331e0285cade5391
Garrett Hough <GarrettHough2004@gmail.com>
2025-07-10 09:28:46 -0600

    Update ShoppingStoreButtons.gd

--- cookingupsuccessgame/Scripts/ShoppingStoreButtons.gd
+++ cookingupsuccessgame/Scripts/ShoppingStoreButtons.gd
@@ -42,16 +42,16 @@ func _on_back_pressed() -> void:
 	var screen_width: float = get_viewport().get_visible_rect().size.x
 	print("back button was pressed")
 
-	if aisle_index > 0:
+	if aisle_index >= 0 && aisle_index <= 2:
 		aisle_index -= 1
 		camera.position.x = aisle_index * screen_width
 
 func _on_next_pressed() -> void:
 	var screen_width: float = get_viewport().get_visible_rect().size.x
 	print("next button was pressed")
-
-	aisle_index += 1  # increase aisle
-	camera.position.x = aisle_index * screen_width
+	if aisle_index >= 0 && aisle_index <= 2:
+		aisle_index += 1  # increase aisle
+		camera.position.x = aisle_index * screen_width
 
 func _on_finished_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/cooking.tscn")
